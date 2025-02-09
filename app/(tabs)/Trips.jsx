b44788c9feaa6896b439530760ac3c4e99e159ed
suyashpotdar <suyashpotdar03@gmail.com>
2025-02-09 15:05:10 +0530

    allow the trip page to display the trip details

--- app/(tabs)/Trips.jsx
+++ app/(tabs)/Trips.jsx
@@ -1,14 +1,51 @@
-import { View, Text } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { Colors } from "../../constants/Colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import StartNewTripCard from "../../components/MyTrips/StartNewTripCard";
+import { collection, doc, getDocs, query, where } from "firebase/firestore";
+import { auth, db } from "../../configs/FirebaseConfig";
+import UserTripList from "../../components/MyTrips/UserTripList";
+import { useRouter } from "expo-router";
 
 export default function Trips() {
   const [userTrips, setUserTrips] = useState([]);
+  const user = auth.currentUser;
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    user && GetMyTrips();
+  }, [user]);
+
+  const GetMyTrips = async () => {
+    setLoading(true);
+    setUserTrips([]);
+    const q = query(
+      collection(db, "UserTrips"),
+      where("userEmail", "==", user?.email)
+    );
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots.
+      console.log(doc.id, " => ", doc.data());
+      setUserTrips((prev) => [...prev, doc.data()]);
+    });
+    setLoading(false);
+  };
 
   return (
-    <View
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1, paddingBottom: 20 }} // Allows scrolling if content is smaller than the screen
+      keyboardShouldPersistTaps="handled" // Ensures touch events work inside ScrollView
+      showsVerticalScrollIndicator={false} // Optional: Hides the scrollbar
       style={{
         padding: 20,
         paddingTop: 55,
@@ -32,10 +69,19 @@ export default function Trips() {
         >
           My Trips
         </Text>
-        <Ionicons name="add-circle-outline" size={50} color="black" />
+        <TouchableOpacity
+          onPress={() => router.push("/create_trip/search_place")}
+        >
+          <Ionicons name="add-circle-outline" size={50} color="black" />
+        </TouchableOpacity>
       </View>
+      {loading && <ActivityIndicator size={"large"} color={Colors.PRIMARY} />}
 
-      {userTrips?.length == 0 ? <StartNewTripCard /> : null}
-    </View>
+      {userTrips?.length == 0 ? (
+        <StartNewTripCard />
+      ) : (
+        <UserTripList userTrips={userTrips} />
+      )}
+    </ScrollView>
   );
 }
---
+++ components/MyTrips/UserTripCard.jsx
@@ -0,0 +1,65 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import moment from "moment";
+
+export default function UserTripCard({ trip }) {
+  const formatData = (data) => {
+    return JSON.parse(data);
+  };
+  return (
+    <View
+      style={{
+        marginTop: 20,
+        display: "flex",
+        flexDirection: "row",
+        gap: 10,
+        alignItems: "center",
+      }}
+    >
+      {/*<Image
+        source={require("../../assets/images/login.jpg")}
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 15,
+        }}
+      />*/}
+      <Image
+        source={{
+          uri:
+            "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=" +
+            formatData(trip.tripData).locationInfo?.photoRef +
+            "&key=" +
+            process.env.EXPO_PUBLIC_GOOGLE_MAP_KEY,
+        }}
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 15,
+        }}
+      />
+      <View style={{ flex: 1, marginRight: 10 }}>
+        <Text
+          style={{
+            fontFamily: "outfit-medium",
+            fontSize: 18,
+            flexShrink: 1, // Prevents overflow and allows shrinking
+          }}
+          numberOfLines={1} // Keeps text to a single line and truncates with "..."
+          ellipsizeMode="tail" // Adds "..." at the end if text overflows
+        >
+          {trip.tripPlan?.travelPlan?.location}
+        </Text>
+        <Text
+          style={{
+            fontFamily: "outfit",
+            fontSize: 14,
+            color: "#7d7d7d",
+          }}
+        >
+          {moment(formatData(trip.tripData).startDate).format("DD MMM YYYY")}
+        </Text>
+      </View>
+    </View>
+  );
+}
